7974777421c347c640386eaadb4c112e7b898870
Stephanie Velazquez <svelazquez@Stephanies-MacBook-Air.local>
2018-09-19 14:56:27 -0500

    added assets

--- js/init.js
+++ js/init.js
@@ -1,8 +1,20 @@
 (function($){
+
   $(function(){
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+
+      var animation = "animated flipInX";
+      var animationEnd = 'animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd';
+
+
+    $("#intro-button").click(function(){
+      $("#intro>*").addClass(animation).one(animationEnd,
+          function(){
+        $(this).removeClass(animation);
+          });
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
